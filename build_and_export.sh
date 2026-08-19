75c035aa698783ce536ba3cb2e653fc0cfb1e7b4
Rubén Trujillo <ruben@doweb.mx>
2026-08-18 22:38:15 -0600

    🚧  Update branding

--- build_and_export.sh
+++ build_and_export.sh
@@ -1,2 +1,13 @@
-docker build -t dowebmx/docuseal:latest .
-docker save -o dowebmx-docuseal.tar dowebmx/docuseal:latest
+docker image ls
+
+docker image rm -f dowebmx/docuseal:latest
+
+docker image prune -f
+docker builder prune -f
+docker system prune -a -f
+# docker build --no-cache -t dowebmx/docuseal:latest .
+docker build --no-cache --pull -t dowebmx/docuseal:latest .
+
+docker save -o ./target/dowebmx-docuseal.tar dowebmx/docuseal:latest
+
+docker image ls
