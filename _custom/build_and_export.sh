5f973f9049e7a0f2a079496b6902e2b8f77ae942
Rubén Trujillo <ruben@doweb.mx>
2026-09-01 12:39:50 -0600

    📦 Update core version

--- _custom/build_and_export.sh
+++ _custom/build_and_export.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+# CREATED BY RUBEN TRUJILLO - ABR 01 2026
+# UPDATED BY RUBEN TRUJILLO - SEP 01 2026
+clear
 
-docker image ls
+# VARIABLES
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ENV_FILE="$SCRIPT_DIR/.env"
+source "$ENV_FILE"
 
-docker image rm -f dowebmx/docuseal:latest
+echo "----------------------------------------------"
+echo " DOWEBMX DOCUSEAL BUILD IMAGE STARTED "
+echo "----------------------------------------------"
+
+# docker image ls
+# docker image rm -f dowebmx/docuseal
+
+echo "v${DOCUSEAL_VERSION}-DoWebMx-rev-${REVISION}" > ./version
 
 docker image prune -f
 docker builder prune -f
 docker system prune -a -f
 
 # Opción limpia: Detiene y elimina solo si existen contenedores previos
-docker ps -a -q --filter ancestor=dowebmx/docuseal:latest | xargs -r docker stop
-docker ps -a -q --filter ancestor=dowebmx/docuseal:latest | xargs -r docker rm
+docker ps -a -q --filter ancestor=dowebmx/docuseal | xargs -r docker stop
+docker ps -a -q --filter ancestor=dowebmx/docuseal | xargs -r docker rm
 
 # docker build --no-cache --pull -t dowebmx/docuseal:latest .
 # docker build --pull --no-cache -f Dockerfile.custom -t dowebmx/docuseal:latest .
@@ -20,11 +33,20 @@ docker build --pull --no-cache \
   --build-arg APP_DESCRIPTION="Plataforma para la gestión, validación y firma electrónica de documentos digitales." \
   --build-arg APP_URL="https://www.doweb.mx" \
   -f Dockerfile.custom \
+  -t dowebmx/docuseal:${DOCUSEAL_VERSION}-DoWebMx-rev-${REVISION} \
   -t dowebmx/docuseal:latest \
   ..
 
 docker save -o ./target/dowebmx-docuseal.tar dowebmx/docuseal:latest
+# docker save -o dowebmx-erp.tar dowebmx/cloud-erp:${DOLI_VERSION}-php${PHP_VERSION}-rev${REVISION} dowebmx/cloud-erp:latest
 
 docker image ls
 
+rm version
+
 # docker run --rm dowebmx/docuseal:latest cat /app/app/views/pages/landing.html.erb
+# docker run --rm dowebmx/docuseal:latest cat /app/app/views/shared/_powered_by.html.erb
+
+echo "------------------------------------------------"
+echo " DOWEBMX DOCUSEAL BUILD IMAGE COMPLETED "
+echo "------------------------------------------------"
